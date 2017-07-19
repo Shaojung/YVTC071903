f7eb974430c609ce2e18bcfff7077dae57363da5
Shaojung <shaojung@gmail.com>
2017-07-19 16:12:29 +0800

    put open data into ListView

--- app/src/main/java/com/example/yvtc/yvtc071903/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc071903/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.yvtc.yvtc071903;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -11,21 +15,58 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> mylist;
+    ListView lv;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mylist = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
+                    mylist);
+        lv.setAdapter(adapter);
     }
-    public void click1(View v)
-    {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("Get Data");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-        StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
+        final StringRequest request = new StringRequest("http://data.ntpc.gov.tw/od/data/api/BF90FA7E-C358-4CDA-B579-B6C84ADC96A1?$format=json",
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.d("NET", response);
+                        try {
+                            mylist.clear();
+                            JSONArray array = new JSONArray(response);
+                            int i;
+                            for (i=0;i<array.length();i++)
+                            {
+                                JSONObject obj = array.getJSONObject(i);
+                                String str = obj.get("district").toString();
+                                Log.d("NET", str);
+                                mylist.add(str);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    adapter.notifyDataSetChanged();
+
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -35,5 +76,7 @@ public class MainActivity extends AppCompatActivity {
         });
         queue.add(request);
         queue.start();
+        return super.onOptionsItemSelected(item);
     }
+
 }
